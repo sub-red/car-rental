d73137977e42ff2dbfbda51a1d973414223870bd
gilaseillgren <33657449+gilaseillgren@users.noreply.github.com>
2020-02-17 09:52:05 +0100

    added search for tables and fontawesome icons

--- CarRentalApplication/CarRental.MVC/wwwroot/js/site.js
+++ CarRentalApplication/CarRental.MVC/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $(".searchInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".searchTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
